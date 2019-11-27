ea3961257158bf407239aaf35ea54250726bf086
DLR-TK <biemingjie@163.com>
2019-11-28 00:00:17 +0800

    Stack ArrayStack 基于动态数组实现栈 Array update

--- src/main/java/com/tk/datastructure/array/Array.java
+++ src/main/java/com/tk/datastructure/array/Array.java
@@ -97,6 +97,14 @@ public class Array<E> {
         return data[index];
     }
 
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
     /**
      * 修改索引为index的元素为e
      * @param index 索引
