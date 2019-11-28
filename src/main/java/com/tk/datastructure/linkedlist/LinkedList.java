35be3ac6820a0327bbb45bcdef070c142863495f
DLR-TK <biemingjie@163.com>
2019-11-28 23:49:34 +0800

    链表

---
+++ src/main/java/com/tk/datastructure/linkedlist/LinkedList.java
@@ -0,0 +1,193 @@
+package com.tk.datastructure.linkedlist;
+
+public class LinkedList<E> {
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    /**
+     * 返回链表长度
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 判断是否为空
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 向链表中插入节点（练习）
+     */
+    public void add(int index, E e) {
+
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index ; i++) {
+            prev = prev.next;
+        }
+//        Node node = new Node(e);
+//        node.next = prev.next;
+//        prev.next = node;
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    /**
+     * 向链表头部插入节点
+     */
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    /**
+     * 向链表尾部插入节点
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    /**
+     * 获得链表的第index位置元素（练习）
+     */
+    public E get(int index) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    /**
+     * 获得链表的第一个元素
+     */
+    public E getFirst() {
+        return get(0);
+    }
+
+    /**
+     * 获得链表的最后一个元素
+     */
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    /**
+     * 修改链表的第index元素为e（练习）
+     */
+    public void set(int index, E e) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否存在e
+     */
+    public boolean contains(E e) {
+
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder res = new StringBuilder();
+
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            res.append(cur.e + "->");
+//            cur = cur.next;
+//        }
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            res.append(cur.e + "->");
+        }
+        res.append("NULL");
+
+        return res.toString();
+    }
+
+    /**
+     * 从链表中删除第index位置元素 返回删除元素（练习）
+     */
+    public E remove(int index) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Illegal index.");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+
+    /**
+     * 从链表中删除第一个元素 返回删除元素（练习）
+     */
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    /**
+     * 从链表中删除最后一个元素 返回删除元素（练习）
+     */
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
